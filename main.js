c7ee936524209a9994d0f927eb64b127bfc80570
David Lau <David.lau@codeimmersives.com>
2020-02-06 14:30:07 -0500

    added more randomness

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 matchtasic = (name, name2) => {
 
 
+    let letter = /[a-zA-Z]/
+    
+let randomnoun = Math.floor(Math.random()*3)
 let randomnumber = Math.floor(Math.random()*101)
 
 let prediction = name + ' & ' + name2 + " have a " + randomnumber + "% Match-tastic rating, I suggest you"
+
+
+if (name.match(letter) === null ) {
+ return 'thats not a name u idiot'
+}
 if (randomnumber === 0){
     prediction +=  " run Away!"
     return prediction
 }if (randomnumber >= 1 && randomnumber <= 25 ) {
-    prediction +=  " just stay friends"
+    let noun = ''
+    switch (randomnoun){
+    case 0:
+    noun = 'friends'
+    break
+    case 1:
+    noun = 'buddies'
+    break
+    case 2 :
+    noun = 'coffee buddies'
+    break
+    case 3 :
+    noun = 'study partners'
+    break;
+    }
+   prediction +=  " just stay " + noun
     return prediction
+
 }if (randomnumber >= 26 && randomnumber <= 50) {
     prediction +=  " steer clear from this possible Nightmare "
     return prediction
@@ -30,23 +54,6 @@ if (randomnumber === 0){
 
 
 
- 
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 }
