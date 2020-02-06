24c7280913ad79003bf42398ada0881b43ef8893
David Lau <David.lau@codeimmersives.com>
2020-02-06 12:24:16 -0500

    added few lines of code

---
+++ main.js
@@ -0,0 +1,37 @@
+matchtasic = (name,name2) => {
+
+let randomnumber = Math.floor(Math.random()*101)
+
+
+if (randomnumber === 0){
+
+}if (randomnumber >= 1 && randomnumber <= 25 ) {
+
+}if (randomnumber >= 26 && randomnumber <= 50) {
+
+}if ( randomnumber >=51 && randomnumber <= 75) {
+
+}if (randomnumber >= 76 && randomnumber <= 99) {
+    
+}if (randomnumber === 100) {
+
+}else{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
