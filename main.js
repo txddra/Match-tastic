b7116d57c040ca9a5909ff844f3e51c0463ef340
David Lau <David.lau@codeimmersives.com>
2020-02-06 12:43:32 -0500

    updated with prediction

--- main.js
+++ main.js
@@ -2,20 +2,25 @@ matchtasic = (name,name2) => {
 
 let randomnumber = Math.floor(Math.random()*101)
 
-let prediction = name + ' & ' + name2 + " have a " + randomnumber + "% Match-tastic rating"
+let prediction = name + ' & ' + name2 + " have a " + randomnumber + "% Match-tastic rating, I suggest you"
 if (randomnumber === 0){
-  return "Run Away!"
+    prediction +=  "Run Away!"
+    return prediction
 }if (randomnumber >= 1 && randomnumber <= 25 ) {
-  return "Just stay friends"
+    prediction +=  "Just stay friends"
+    return prediction
 }if (randomnumber >= 26 && randomnumber <= 50) {
-  return "Possible Nightmare "
+    prediction +=  "Possible Nightmare "
+    return prediction
 }if ( randomnumber >=51 && randomnumber <= 75) {
-  return "Test your fate, this could be great"
+    prediction +=  "Test your fate, this could be great"
+    return prediction
 }if (randomnumber >= 76 && randomnumber <= 99) {
-  return "A fairytale, perhaps!"  
+  prediction += "A fairytale, perhaps!" 
+  return prediction 
 }if (randomnumber === 100) {
   prediction += "A match made in heaven"
-
+  return prediction
 }else{
     
 }
