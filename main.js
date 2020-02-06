0c847aa01cfae19bd3c1b2ac25956a584d510daa
Your Name <you@example.com>
2020-02-06 12:54:02 -0500

    fix?

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-matchtasic = (name,name2) => {
+matchtasic = (name, name2) => {
+
 
 let randomnumber = Math.floor(Math.random()*101)
 
 let prediction = name + ' & ' + name2 + " have a " + randomnumber + "% Match-tastic rating"
 if (randomnumber === 0){
-  return "Run Away!"
+    return "Run Away!"
 }if (randomnumber >= 1 && randomnumber <= 25 ) {
-  return "Just stay friends"
+    return "Just stay friends"
 }if (randomnumber >= 26 && randomnumber <= 50) {
-  return "Possible Nightmare "
+    return "Possible Nightmare "
 }if ( randomnumber >=51 && randomnumber <= 75) {
-  return "Test your fate, this could be great"
+    return "Test your fate, this could be great"
 }if (randomnumber >= 76 && randomnumber <= 99) {
-  return "A fairytale, perhaps!"  
+    return "A fairytale, perhaps!"  
 }if (randomnumber === 100) {
-  prediction += "A match made in heaven"
+prediction += "A match made in heaven"
 
 }else{
     
@@ -23,7 +24,7 @@ if (randomnumber === 0){
 
 
 
-    
+
 
 
 
